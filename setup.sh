dc5770ff7422cf865b268d64a78e1509bdd0b959
Rui <rmrbranco@riseup.net>
2022-12-15 22:17:20 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "###############################################"
-echo "### Welcome to the dht22-sensor setup       ###"
-echo "###############################################"
-echo
-echo
 echo
 echo "Setup proceeding in 10 seconds... (control-c to cancel)"
 echo
@@ -18,10 +13,15 @@ sudo true
         build-essential \
         python3 \
         python3-dev \
-        python3-pip \
-        gnuplot-x11
-    sudo yes | python3 -m pip install --upgrade pip setuptools wheel
+        python3-pip
+        gnuplot
+
+    sudo python3 -m yes | pip install --upgrade pip setuptools wheel
     sudo yes | pip3 install --install-option="--force-pi" Adafruit_DHT
+
+(crontab -l ; echo "@reboot cd /home/$USER/dht22_server/ && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "*/30 * * * * cd /home/$USER/dht22_server/ && python3 dht22logger.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "59 23 * * 0 cd /home/<$USER>/dht22_server/ && cp plot.png weather_history/plot_"$(date '+%m%d%Y').png"") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
 echo
 echo
