3f16c10e949b7cabea0142eb60ba4a6763f0555b
Rui <rmrbranco@riseup.net>
2022-12-20 23:58:41 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,11 +19,11 @@ sudo true
     sudo yes | python3 -m pip install --upgrade pip setuptools wheel
     sudo yes | pip3 install --install-option="--force-pi" Adafruit_DHT
 
-(crontab -l ; echo "@reboot cd /home/$USER/dht22_server/ && echo "[`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Coming from the reboot." >> dht22server_log.txt && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "*/30 * * * * cd /home/$USER/dht22_server/ && python3 dht22logger.py && echo "[`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Logging." >> dht22server_log.txt") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "59 23 * * 0 cd /home/$USER/dht22_server/ && cp plot.png weather_history/plot_"`date "+\%d-\%m-\%Y_\%H:\%M:\%S"`.png" && echo "[`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Save weather history." >> dht22server_log.txt") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "15 1 * * * pkill -f dht22server && cd /home/$USER/dht22_server/ && echo "[`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Restart server." >> dht22server_log.txt && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22_server/ && cp dht22server_log.txt logs_history/dht22server_log_"`date "+\%d-\%m-\%Y"`.txt" && echo -n "" > dht22server_log.txt && echo "[`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Started rotate log files." >> dht22server_log.txt") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "@reboot cd /home/$USER/dht22_server/ && echo "`hostname -I | cut -f1 -d ' '` - - [`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Coming from the reboot." >> dht22server_log.txt && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "*/30 * * * * cd /home/$USER/dht22_server/ && python3 dht22logger.py && echo "`hostname -I | cut -f1 -d ' '` - - [`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Logging." >> dht22server_log.txt") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "59 23 * * 0 cd /home/$USER/dht22_server/ && cp plot.png weather_history/plot_"`date "+\%d-\%m-\%Y_\%H:\%M:\%S"`.png" && echo "`hostname -I | cut -f1 -d ' '` - - [`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Save weather history." >> dht22server_log.txt") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "15 1 * * * pkill -f dht22server && cd /home/$USER/dht22_server/ && echo "`hostname -I | cut -f1 -d ' '` - - [`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Restart server." >> dht22server_log.txt && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22_server/ && cp dht22server_log.txt logs_history/dht22server_log_"`date "+\%d-\%m-\%Y"`.txt" && echo -n "" > dht22server_log.txt && echo "`hostname -I | cut -f1 -d ' '` - - [`date "+\%d/\%b/\%Y \%H:\%M:\%S"`] Started rotate log files." >> dht22server_log.txt") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
 echo
 echo
