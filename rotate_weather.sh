53dd3b2341dc158e00f09f835328bfc270eb07fe
Rui <rmrbranco@riseup.net>
2022-12-22 01:00:24 +0000

    Delete rotate_weather.sh

--- rotate_weather.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd /home/$USER/dht22_server/
-cp plot.png weather_history/plot_"`date "+%d-%m-%Y"`.png"
-echo "`hostname -I | cut -f1 -d ' '` - - [`date "+%d/%b/%Y %H:%M:%S"`] Rotate weather history log." >> dht22server_log.txt
-
