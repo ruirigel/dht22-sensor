eb8c7c20d47e66de41727a79899a04366c5fd1f3
Rui <rmrbranco@riseup.net>
2022-12-22 01:00:06 +0000

    Delete start_dht22server.sh

--- start_dht22server.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd /home/$USER/dht22_server/
-echo "`hostname -I | cut -f1 -d ' '` - - [`date "+%d/%b/%Y %H:%M:%S"`] Coming from the reboot." >> dht22server_log.txt
-python3 dht22server.py
