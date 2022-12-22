755ef996d80b0c5ed57db0b4fa49accf82b5e1f3
Rui <rmrbranco@riseup.net>
2022-12-22 01:02:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,13 +19,13 @@ sudo true
     sudo yes | python3 -m pip install --upgrade pip setuptools wheel
     sudo yes | pip3 install --install-option="--force-pi" Adafruit_DHT
 
-(crontab -l ; echo "@reboot cd /home/$USER/dht22_server/ && ./start_dht22server.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "*/30 * * * * cd /home/$USER/dht22_server/ && ./check_dht22sensor.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "59 23 * * 0 cd /home/$USER/dht22_server/ && ./rotate_weather.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "*/5 * * * * cd /home/$USER/dht22_server/ && ./check_dht22server.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22_server/ && ./rotate_logs.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "@reboot cd /home/$USER/dht22_server/bin/ && ./start_dht22server.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "*/30 * * * * cd /home/$USER/dht22_server/bin/ && ./check_dht22sensor.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "59 23 * * 0 cd /home/$USER/dht22_server/bin/ && ./rotate_weather.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "*/5 * * * * cd /home/$USER/dht22_server/bin/ && ./check_dht22server.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22_server/bin/ && ./rotate_logs.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
-cd /home/$USER/dht22_server/
+cd /home/$USER/dht22_server/bin/
 chmod +x start_dht22server.sh check_dht22sensor.sh rotate_weather.sh check_dht22server.sh rotate_logs.sh
 
 echo
