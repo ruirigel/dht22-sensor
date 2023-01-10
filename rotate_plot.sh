640d13e416e637923804677c2e19b7088b185a07
Rui <rmrbranco@riseup.net>
2023-01-10 23:50:45 +0000

    Create rotate_plot.sh

---
+++ rotate_plot.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cp plot.txt plot_history/plot_"`date "+%d-%m-%Y"`.txt"
+echo -n "" > plot.txt
+echo "`hostname -I | cut -f1 -d ' '` - - [`date "+%d/%b/%Y %H:%M:%S"`] Started rotate plot files." >> dht22server_log.txt
