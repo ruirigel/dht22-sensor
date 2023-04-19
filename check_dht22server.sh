c642e8d1c63568371ea81b20dd0684730fd81435
Rui <rmrbranco@riseup.net>
2023-04-19 22:10:16 +0100

    Update check_dht22server.sh

--- check_dht22server.sh
+++ check_dht22server.sh
@@ -2,9 +2,11 @@
 
 # 20230327 Rui Rigel
 
-echo "`hostname -I | cut -f1 -d ' '` - - [`date "+%d/%b/%Y %H:%M:%S"`] Checking server." >> dht22server_log.txt
+if pgrep -f "check_dht22_server.sh" > /dev/null ; then
+        pkill -f check_dht22_server.sh
+fi
 
-code=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8000/plot.png)
+code=$(curl --write-out %{http_code} --silent --connect-timeout 10 --output /dev/null localhost:8000/plot.png)
 
 if [[ "$code" -ne 200 ]] ; then
 	if pgrep -f "dht22server.py" > /dev/null ; then
