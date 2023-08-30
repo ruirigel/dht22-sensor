eccf9efab694493ad6ea7b3731955aa57f1e6177
Rui <rmrbranco@riseup.net>
2023-08-30 16:58:36 +0100

    Update setup.sh
    fix location of main folder

--- setup.sh
+++ setup.sh
@@ -19,12 +19,12 @@ sudo true
     sudo yes | python3 -m pip install --upgrade pip setuptools wheel
     sudo yes | pip3 install --install-option="--force-pi" Adafruit_DHT
 
-(crontab -l ; echo "@reboot cd /home/$USER/dht22_server/ && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "*/15 * * * * cd /home/$USER/dht22_server/ && python3 dht22logger.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "59 23 * * 0 cd /home/$USER/dht22_server/ && ./rotate_weather.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "*/5 * * * * cd /home/$USER/dht22_server/ && ./check_dht22server.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22_server/ && ./rotate_logs.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22_server/ && ./rotate_plot.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "@reboot cd /home/$USER/dht22-sensor/ && python3 dht22server.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "*/15 * * * * cd /home/$USER/dht22-sensor/ && python3 dht22logger.py") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "59 23 * * 0 cd /home/$USER/dht22-sensor/ && ./rotate_weather.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "*/5 * * * * cd /home/$USER/dht22-sensor/ && ./check_dht22server.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22-sensor/ && ./rotate_logs.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "15 1 1 * * cd /home/$USER/dht22-sensor/ && ./rotate_plot.sh") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
 chmod +x check_dht22server.sh rotate_weather.sh rotate_plot.sh rotate_logs.sh
 
